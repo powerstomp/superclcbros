e15d1a050c2df89c9cb4ddfc433f8f92481541f8
puinyu <119428043+puinyu@users.noreply.github.com>
2024-11-19 09:23:12 +0700

    Provide texture manager service

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <memory>
 
 #include "Game.h"
+#include "Utility/ServiceLocator.h"
+#include "Utility/TextureManager.h"
 
 int main() {
+	ServiceLocator<TextureManager>::Set(std::make_unique<TextureManager>());
+
 	auto game = std::make_unique<Game>();
 	game->Run();
 }
