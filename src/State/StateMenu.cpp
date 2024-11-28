1b8c08143c226cc6d8aade5a7a86dc1e58778586
T1 Thales <gnorteuih122@gmail.com>
2024-11-28 20:36:13 +0700

    Change StateMenu class

--- src/State/StateMenu.cpp
+++ src/State/StateMenu.cpp
@@ -7,27 +7,100 @@
 #include "../Utility/ServiceLocator.h"
 #include "StatePlay.h"
 
-StateMenu::StateMenu(Game* game) : game{game} {
-	titleText.setFont(ServiceLocator<sf::Font>::Get());
-	titleText.setString("Super CLC Bros");
-	titleText.setCharacterSize(50);
-	titleText.setFillColor(sf::Color::White);
+// StateMenu::StateMenu(Game* game) : game{game} {
+// 	titleText.setFont(ServiceLocator<sf::Font>::Get());
+// 	titleText.setString("Super CLC Bros");
+// 	titleText.setCharacterSize(50);
+// 	titleText.setFillColor(sf::Color::White);
 
-	auto viewSize = game->window.getDefaultView().getSize();
-	titleText.setPosition(viewSize.x / 2 - titleText.getGlobalBounds().width / 2, 50);
-}
+// 	auto viewSize = game->window.getDefaultView().getSize();
+// 	titleText.setPosition(viewSize.x / 2 - titleText.getGlobalBounds().width / 2, 50);
+// }
+
+// void StateMenu::OnEnter() {
+// 	game->window.setView(game->window.getDefaultView());
+// }
+// void StateMenu::Update() {
+// }
+
+// void StateMenu::Render(double deltaTime) {
+// 	game->window.draw(titleText);
+// }
+// void StateMenu::OnSFMLEvent(const sf::Event& event) {
+// 	if (event.type == event.KeyPressed)
+// 		if (event.key.code == sf::Keyboard::Key::Enter)
+// 			game->PushState(std::make_unique<StatePlay>(game));
+// }
+
+StateMenu::StateMenu(Game* game) : game{game}
+{}
 
 void StateMenu::OnEnter() {
-	game->window.setView(game->window.getDefaultView());
+    // Load font
+    font = ServiceLocator<sf::Font>::Get();
+
+    // Initialize title text
+    titleText.setFont(font);
+    titleText.setString("Super CLC Bros");
+    titleText.setCharacterSize(50);
+    titleText.setFillColor(sf::Color::White);
+    auto viewSize = game->window.getDefaultView().getSize();
+    titleText.setPosition(viewSize.x / 2 - titleText.getGlobalBounds().width / 2, 50);
+	UpdateOption();
 }
+
+void StateMenu::AddMenuItem(const std::string& label, bool* configValue) {
+	MenuItem item;
+	item.label = label;
+	item.configValue = configValue;
+	item.text.setFont(font);
+	item.text.setString(label);
+	item.text.setCharacterSize(30);
+	item.text.setFillColor(sf::Color::White);
+	items.push_back(item);
+}
+
+void StateMenu::UpdateOption(){
+    float startY = 150.0f;
+    auto viewSize = game->window.getDefaultView().getSize();
+
+    for (size_t i = 0; i < items.size(); ++i) {
+        auto& item = items[i];
+        item.text.setString(item.label + " [" + (*item.configValue ? "On" : "Off") + "]");
+        item.text.setFillColor(i == selectedIndex ? sf::Color::Yellow : sf::Color::White);
+        item.text.setPosition(viewSize.x / 2 - item.text.getGlobalBounds().width / 2, startY + i * 40.0f);
+    }
+}
+
 void StateMenu::Update() {
 }
 
 void StateMenu::Render(double deltaTime) {
+	game->window.clear(sf::Color::Black);
 	game->window.draw(titleText);
+	for (const auto& item : items) {
+		game->window.draw(item.text);
+	}
 }
+
 void StateMenu::OnSFMLEvent(const sf::Event& event) {
-	if (event.type == event.KeyPressed)
-		if (event.key.code == sf::Keyboard::Key::Enter)
-			game->PushState(std::make_unique<StatePlay>(game));
+	if (event.type == sf::Event::KeyPressed) {
+		if (event.key.code == sf::Keyboard::Key::Up) {
+			if (selectedIndex > 0) {
+				--selectedIndex;
+			}
+		}
+		else if (event.key.code == sf::Keyboard::Key::Down) {
+			if (selectedIndex < items.size() - 1) {
+				++selectedIndex;
+			}
+		}
+		else if (event.key.code == sf::Keyboard::Key::Enter) {
+			if (selectedIndex < items.size()) {
+				*items[selectedIndex].configValue = !*items[selectedIndex].configValue;
+			}
+		}
+		UpdateOption();
+	}
 }
+
--- src/State/StateMenu.h
+++ src/State/StateMenu.h
@@ -1,20 +1,39 @@
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/Graphics/Text.hpp>
-#include <SFML/Graphics/View.hpp>
+#ifndef STATEMENU_H
+#define STATEMENU_H
 
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <vector>
+#include <functional>
 #include "../Game.h"
 #include "State.h"
 
 class StateMenu : public State {
-private:
-	Game* game;
-	sf::Text titleText;
+protected:
+    Game* game;
+    sf::Font font;
+    sf::Text titleText;
+
+    struct MenuItem {
+        std::string label;          
+        bool* configValue;           
+        sf::Text text;               
+    };
+
+    std::vector<MenuItem> items;     
+    size_t selectedIndex = 0;       
+
+    void UpdateOption();
 
 public:
-	StateMenu(Game*);
+    StateMenu(Game* game);
 
-	virtual void OnEnter() override;
-	virtual void Update() override;
-	virtual void Render(double deltaTime) override;
-	virtual void OnSFMLEvent(const sf::Event&) override;
+    virtual void OnEnter() override;
+    virtual void Update() override;
+    virtual void Render(double deltaTime) override;
+    virtual void OnSFMLEvent(const sf::Event& event) override;
+
+    void AddMenuItem(const std::string& label, bool* configValue);
 };
+
+#endif 
